36664ff6d61ddec2f97d1dcb94051a74da2b3f43
tiedsandi <fachransandi@gmail.com>
2024-06-22 10:24:06 +0700

    adding a post grid and post items

---
+++ components/home-page/featured-posts.js
@@ -0,0 +1,11 @@
+import classes from './featured-posts.module.css';
+
+function FeateruredPosts() {
+  return (
+    <section className={classes.latest}>
+      <h2>Featured Posts</h2>
+    </section>
+  );
+}
+
+export default FeateruredPosts;
