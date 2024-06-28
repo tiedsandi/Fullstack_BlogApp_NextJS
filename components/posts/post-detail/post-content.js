6e059a64bb5e1b3850fd9b86422fb24fcdb4a46a
tiedsandi <fachransandi@gmail.com>
2024-06-28 08:50:35 +0700

    rendering image with the "Next Image" Component (from markdown)

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,4 +1,5 @@
 import Markdown from 'react-markdown';
+import Image from 'next/image';
 
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
@@ -7,10 +8,35 @@ function PostContent(props) {
   const {post} = props;
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+  const customComponents = {
+    // img: (props) => {
+    //   const {src, alt} = props;
+    //   return <Image src={`/images/posts/${post.slug}/${src}`} alt={alt} width={600} height={300} />;
+    // },
+    p: (props) => {
+      const {children} = props;
+
+      if (children.type === 'img') {
+        const {src, alt} = children.props;
+
+        return (
+          <div className={classes.image}>
+            <Image src={`/images/posts/${post.slug}/${src}`} alt={alt} width={600} height={300} />;
+          </div>
+        );
+      }
+      return <p>{children}</p>;
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <Markdown>{post.content}</Markdown>
+      <Markdown
+        // customComponents={customComponents}
+        components={customComponents}>
+        {post.content}
+      </Markdown>
     </article>
   );
 }
